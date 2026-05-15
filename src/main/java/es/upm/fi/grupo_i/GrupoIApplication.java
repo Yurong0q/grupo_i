a6edc706a6d66099d5c7765e216ba86c46c2c3f6
Yurong0q <151645375+Yurong0q@users.noreply.github.com>
2026-05-15 14:10:26 +0200

    Primer commit

---
+++ src/main/java/es/upm/fi/grupo_i/GrupoIApplication.java
@@ -0,0 +1,13 @@
+package es.upm.fi.grupo_i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoIApplication.class, args);
+	}
+
+}
