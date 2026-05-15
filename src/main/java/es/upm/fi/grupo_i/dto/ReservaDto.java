42c36a3acb16cf8f4c580534eab95b9436ee97b4
Yurong0q <151645375+Yurong0q@users.noreply.github.com>
2026-05-15 21:49:11 +0200

    fixed: dto now record instead of class

--- src/main/java/es/upm/fi/grupo_i/dto/ReservaDto.java
+++ src/main/java/es/upm/fi/grupo_i/dto/ReservaDto.java
@@ -2,10 +2,10 @@ package es.upm.fi.grupo_i.dto;
 
 import es.upm.fi.grupo_i.enums.ESTADO_RESERVA;
 
-public class ReservaDto {
-    private Long id;
-    private Long idViaje;
-    private Long idUsuario;
-    private int numPasajeros;
-    private ESTADO_RESERVA estado;
-}
+public record ReservaDto (
+    Long id,
+    Long idViaje,
+    Long idUsuario,
+    int numPasajeros,
+    ESTADO_RESERVA estado
+){}
--- src/main/java/es/upm/fi/grupo_i/dto/ViajeDto.java
+++ src/main/java/es/upm/fi/grupo_i/dto/ViajeDto.java
@@ -7,16 +7,16 @@ import java.util.List;
 import es.upm.fi.grupo_i.model.Precio;
 import es.upm.fi.grupo_i.enums.ESTADO_VIAJE;
 
-public class ViajeDto {
-    private Long id;
-    private Long conductorId;
-    private String origen;
-    private String destino;
-    private List<String> paradas;
-    private int plazasDisponibles;
-    private Precio precio;
-    private LocalDate fechaSalida;
-    private LocalTime horaSalida;
-    private int duracionEstimada;
-    private ESTADO_VIAJE estado;
-}
+public record ViajeDto (
+    Long id,
+    Long conductorId,
+    String origen,
+    String destino,
+    List<String> paradas,
+    int plazasDisponibles,
+    Precio precio,
+    LocalDate fechaSalida,
+    LocalTime horaSalida,
+    int duracionEstimada,
+    ESTADO_VIAJE estado
+) {}
