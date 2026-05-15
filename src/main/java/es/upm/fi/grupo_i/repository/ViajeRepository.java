f2a3ca960cf55256a04ab33779c9636bb2db33db
AlvaroVicente28 <183276031+AlvaroVicente28@users.noreply.github.com>
2026-05-15 16:11:55 +0200

    added ViajeRepository

--- src/main/java/es/upm/fi/grupo_i/repository/ViajeRepository.java
+++ src/main/java/es/upm/fi/grupo_i/repository/ViajeRepository.java
@@ -1,5 +1,11 @@
 package es.upm.fi.grupo_i.repository;
 
-public class ViajeRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import es.upm.fi.grupo_i.model.Viaje;
+
+public interface ViajeRepository extends JpaRepository<Viaje, Long> {
+    Page<Viaje> findByNombreContainingIgnoreCase(String nombre, Pageable pageable);
 }
--- src/main/java/es/upm/fi/grupo_i/service/ViajeService.java
+++ src/main/java/es/upm/fi/grupo_i/service/ViajeService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import es.upm.fi.grupo_i.model.Viaje;
-// import es.upm.fi.grupo_i.repository.ViajeRepository;
 import es.upm.fi.grupo_i.repository.ViajeRepository;
 
 @Service
@@ -21,5 +20,31 @@ public class ViajeService {
 
     public ViajeService(ViajeRepository ViajeRepository) {
         this.ViajeRepository = ViajeRepository;
+    }
+
+    // Todos los viajes
+    public List<Viaje> obtenerTodosViajes() {
+        return new ArrayList<>(ViajeRepository.findAll());
+    }
+
+    public Page<Viaje> obtenerTodosViajes(String nombre, Pageable pageable) {
+        if (nombre != null && !nombre.trim().isEmpty()) {
+            return ViajeRepository.findByNombreContainingIgnoreCase(nombre, pageable);
+        }
+        return ViajeRepository.findAll(pageable);
+    }
+
+    public Optional<Viaje> obtenerViaje(long id) {
+        return ViajeRepository.findById(id);
+    }
+
+    public void anadirViaje(Viaje viaje) throws Exception {
+        if (viaje.getId() != null) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Un viaje nuevo no debe tener ID"
+            );
+        }
+        ViajeRepository.save(viaje);
     }
 }
